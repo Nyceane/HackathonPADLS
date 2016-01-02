81f8e522de7379dbb4e1b1502c5874cd4050e36a
Nyceane <dotnetpete@gmail.com>
2016-01-02 12:29:39 -0800

    first commit

---
+++ ParseStarterProject/src/main/java/com/parse/starter/TriggerActivity.java
@@ -0,0 +1,16 @@
+package com.parse.starter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by peterma on 1/2/16.
+ */
+public class TriggerActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
